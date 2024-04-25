957286ea249359ec13d20f13e8a64d94d36c433f
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-04-25 20:43:56 +0200

    +method allTask

--- app/models/ModelTask.php
+++ app/models/ModelTask.php
@@ -13,6 +13,15 @@ class ModelTask extends Model
     }
 
 // declarar un array que converta los datos json en php.
+    function allTask() : array
+    {
+        $data = [];
+        $json = json_decode(file_get_contents($this->dbJson)); //convierte los datos json en php.
+        foreach ($json as $row) {  //convierte todos los datos a una array asociativa. 
+            $data[$row->id] = $row;
+        }
+        return $data;
+    }
 // declarar method incremente el id.. (o lo podem incrementar al crear la task, o incrementar x un metodo).
 // declarar method crear task.
 // declarar method ver task.
