8226f73fe8882945bebc3326af270bc0ecb8378e
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-04-25 20:38:08 +0200

    App require

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -1,9 +1,16 @@
 <?php
 
+//llamar a la carpte model archivo.
+require_once(__DIR__ . "/../models/TaskModel.php");
+
 /**
  * Base controller for the application.
  * Add general things in this controller.
+ * testestestesadasdasasdad
  */
+
+ //crear a la class app control segun sale abajo y crear la function construct 
+ //$this->model = new Model() para llamar a la function en el models 
 class ApplicationController extends Controller 
 {
 	
