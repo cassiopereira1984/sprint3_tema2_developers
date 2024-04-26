846b5eb19427729f42966569afe13e93998348bf
MaikGare <maik.fusrodah@gmail.com>
2024-04-26 18:57:05 +0200

    metodo create del controller con comentario 26/04

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -20,4 +20,36 @@ class ApplicationController extends Controller
 
         $this->modelTask = new ModelTask();
     }
+
+    public function createTaskAction()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            //recoger los datos introducidos en formulario de nueva tarea
+            $description = $this->_getParam("description");
+            $status = $this->_getParam("status");
+            $dateIni = date_create()->format('Y-m-d');
+            $dateEnd = date_create($_POST["date_end"])->format('Y-m-d');
+            $user = $this->_getParam("user");
+
+
+            $newTask = [
+                'id' => $this->modelTask->getID(),//hay que crear el metodo getID en ModelTask.php
+                'description' => $description,
+                'status' => $status,
+                'date_ini' => $dateIni,
+                'date_end' => $dateEnd,
+                'user' => $user
+            ];
+
+
+            $this->modelTask->createTask($newTask);
+            header("Location: " . $this->_baseUrl() . "/index");  /*devuelve al usuario a la pagina principal, 
+                                                                    si queremos crear un mensaje de confirmacion 
+                                                                    o algo asi, habría que cambiar el "/index" a 
+                                                                    la ruta de ese script*/
+            exit(); /* Esto asegura que no se ejecute ningún código adicional después de la redirección. */
+        }
+
+
+    }
 }
