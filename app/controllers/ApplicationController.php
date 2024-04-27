8d89c69a63d700f777009c25732958d3e5d6a4f0
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-04-27 18:04:24 +0200

    +method deletetaskaction en controller

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -13,5 +13,16 @@ require_once(__DIR__ . "/../models/TaskModel.php");
  //$this->model = new Model() para llamar a la function en el models 
 class ApplicationController extends Controller 
 {
-	
+
+
+	public function deleteTaskAction(): void
+    {
+        $id = ((int)$this->_getParam('id'));
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["confirm_delete"])) { //verifica si se ha enviado la solicitud
+            $this->modelTask->deleteTask($id); //elimina la task;
+            header("Location: " . $this->_baseUrl());
+            exit();
+        }
+    }
 }
