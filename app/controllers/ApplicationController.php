cd6df32b643a1039e860c7e1f726ded48c59d184
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-04-29 13:53:23 +0200

    +method update controller

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -13,5 +13,27 @@ require_once(__DIR__ . "/../models/TaskModel.php");
  //$this->model = new Model() para llamar a la function en el models 
 class ApplicationController extends Controller 
 {
-	
+	function updateTaskAcciton() 
+    {
+        $task = new ModelTask;
+        $updateTask = [];
+        $id = $_GET["id"]; 
+        $oneTask = $task->readTask($id);
+        $this->view->oneTask=$oneTask; //chequear esta linea;
+        
+        if(!empty($_Post)) {
+            $updateTask =
+            [
+                'id' => (int)$id,
+                'description' => $_POST['description'],
+                'status' => $_POST['status'],
+                'date_ini' => $_POST['dateIni'],
+                'date_end' => $_POST['dateEnd'],
+                'user' => $_POST['user']
+            ];
+            $task->updateTask($updateTask);
+        }
+        return header ('location:./'); //definir ruta de retorno
+        exit();
+    }
 }
